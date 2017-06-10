690bd20a9c01fbf115422e6ade176f3b069773e6
Peter De Genaro <pdegenaro@gmail.com>
2017-06-10 13:40:06 -0400

    deal with stupid shit

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,8 @@
-var path = require("path");
-
 module.exports = {
-  context: __dirname,
-  entry: "./index.js",
-  output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  },
-  devtool: 'source-maps',
-  resolve: {
-    extensions: ["", ".js"]
-  }
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+      path: './bin',
+      filename: 'app.bundle.js'
+    }
 };
